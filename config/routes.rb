0eb4ac7890a06765cd3369461da03e07074fcfc7
1essy <lestia495@yahoo.co.jp>
2022-05-01 13:05:43 +0000

    master.key

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   resources :groups, except: [:destroy]
   resources :groups do     
     get "join" => "groups#join"
-    get "new/mail" => "groups#new_mail"
+    get "new/mail" => "groups#new_mail" 
     get "send/mail" => "groups#send_mail"
   end
   
